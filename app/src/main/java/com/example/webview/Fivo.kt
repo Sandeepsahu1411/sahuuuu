8f25122398241aa3e498a1a96909ca0e9df9918a
Sandeepsahu1411 <sahusandeep26475@gmail.com>
2024-07-25 17:21:41 +0530

    final webview

---
+++ app/src/main/java/com/example/webview/Fivo.kt
@@ -0,0 +1,44 @@
+package com.example.webview
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.KeyEvent
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.webview.databinding.FivoBinding
+
+class Fivo : AppCompatActivity() {
+    private val binding: FivoBinding by lazy {
+        FivoBinding.inflate(layoutInflater)
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.fivo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        setContentView(binding.root)
+
+        binding.webview.loadUrl("https://fivotechnology.com/")
+        binding.webview.settings.javaScriptEnabled =true
+        binding.webview.webViewClient = WebViewClient()
+
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check whether the key event is the Back button and if there's history.
+        if (keyCode == KeyEvent.KEYCODE_BACK && binding.webview.canGoBack()) {
+            binding.webview.goBack()
+            return true
+        }
+        // If it isn't the Back button or there isn't web page history, bubble up to
+        // the default system behavior. Probably exit the activity.
+        return super.onKeyDown(keyCode, event)
+    }
+}
